36898f08f6b2bf25fbd208df0404c7574dcfefe9
Shabana <shabana.sk.ind@gmail.com>
2022-04-03 11:38:47 +0530

    Added Login Feature

---
+++ src/test/java/MyProjectTestCases/LoginPageTest.java
@@ -0,0 +1,11 @@
+package MyProjectTestCases;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is my Login Page Test Class");
+		System.out.println("Here I am testing My Login Page funcionalites");
+
+	}
+
+}
